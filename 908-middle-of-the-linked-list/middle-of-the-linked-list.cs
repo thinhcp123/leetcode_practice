a5a5da0a84808815791a4d5f6f6723d41c88219e
tranducthinh <68468984+thinhcp123@users.noreply.github.com>
2024-06-05 21:37:22 +0700

    Time: 60 ms (51.73%) | Memory: 40 MB (78.50%) - LeetSync

---
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cs
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode MiddleNode(ListNode head) {
+        if(head.next == null) return head;
+        var fast = head;
+        var slow = head;
+        
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
