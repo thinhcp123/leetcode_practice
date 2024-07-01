08b7545b1e83dbce8ca7e8720a0e247b6333c757
tranducthinh <68468984+thinhcp123@users.noreply.github.com>
2024-07-01 09:18:51 +0700

    Time: 123 ms (38.76%) | Memory: 50.7 MB (84.83%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int[] GetConcatenation(int[] nums) {
+        int n = nums.Length;
+        var ans = new int[n * 2];
+        for(int i = 0 ; i < n;i++){
+            ans[i] = nums[i];
+            ans[i + n]= nums[i];
+        }
+        return ans;
+
+    }
+}
